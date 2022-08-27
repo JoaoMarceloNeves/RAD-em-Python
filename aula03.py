8dcb0b6a514640a85ba1a13f0cadd442fbb8b02b
Evandro J.R. Silva <ejrs.profissional@gmail.com>
2022-08-27 14:08:32 -0300

    detalhes

--- aula03.py
+++ aula03.py
@@ -42,27 +42,27 @@
 #   É interessante sempre escrever logo o '.close()', para evitar problemas. Caso a execução seja interrompida, ou fina
 # lizada normalmente, sem que o arquivo seja fechado, você terá problemas para acessá-lo, mesmo com outro programa, como 
 # o bloco de notas.
-#
-##arquivo = open("RAD em Python/teste.txt", "r")
-#
-##print(arquivo.readable())  # imprimindo a verificação se o arquivo pode ser lido
-##print(arquivo.read())      # imprimindo o arquivo
-#
+
+arquivo = open("RAD em Python/teste.txt", "r")
+
+print(arquivo.readable())  # imprimindo a verificação se o arquivo pode ser lido
+print(arquivo.read())      # imprimindo o arquivo
+
 #   readline() lê uma linha e deixa o 'cursor' na linha seguinte. Portanto, uma sequência de readline() vai retornar sem
 # pre a linha seguinte. Com o print(), a leitura de cada linha é impressa. Como cada linha do arquivo que está sendo lido 
 # termina com '\n' (quebra de linha) o resultado de cada readline() vai ter a seguir uma linha em branco.
-#
-##print(arquivo.readline())
-##print(arquivo.readline())
-##print(arquivo.readline())
-#
+
+print(arquivo.readline())
+print(arquivo.readline())
+print(arquivo.readline())
+
 #   A função readlines() retorna uma lista, onde cada elemento da lista é o conteúdo de uma linha. Essa lista está sendo ar
 # mazenada na variável lista. A partir daí podemos manipular a lista. Perceba que manipulando a lista, mesmo que apagando ele
 # mentos, não vai afetar o arquivo.
-#
-##lista = arquivo.readlines()
-##print(lista)
-#
+
+lista = arquivo.readlines()
+print(lista)
+
 #   Outra maneira de imprimir uma lista é utilizando o comando for. Da maneira que foi escrita, este for funciona como o 'for 
 # each' (para cada) de outras linguagens. Então, o código está dizendo mais ou menos assim: para cada elemento da lista, visto 
 # como l : ...
@@ -74,53 +74,53 @@
 # é necessário que aquela linha comece 'tabulada', ou seja, deslocada para o lado. Por isso, o print(l) é visto como uma linha 
 # dentro do for. A primeira após o bloco cujo código não esteja deslocado, o interpretador já vai entender como um comando que não
 # pertence ao for.
-#
-##for l in lista: 
-##    print(l)
-#
-##arquivo.close()
+
+for l in lista: 
+    print(l)
+
+arquivo.close()
 #--------------------------------------------------------------------------------------------------------------
 
 #-- 2 ---------------------------------------------------------------------------------------------------------
-#   Abrinda um arquivo existente com o modo de acesso 'append'. A partir disso é possível acrescentar conteúdo ao arquivo. No 
+#   Abrindo um arquivo existente com o modo de acesso 'append'. A partir disso é possível acrescentar conteúdo ao arquivo. No 
 # nosso caso, criamos um comando para que fosse escrito "\nSQL". Quando o arquivo é aberto em modo 'a' a nova escrita vai ini
 # ciar no espaço seguinte ao último já existente. No nosso exemplo, em teste.txt, tínhamos como último elemento a palavra Matlab.
 # Com o append, a nova escrita começaria colado ao Matlab. Porém, como escrevemos o \n, vai haver primeiro uma quebra de linha. 
 # O SQL é escrito logo após a quebra de linha.
 #
-##arquivo = open("RAD em Python/teste.txt", "a")
-##arquivo.write("\nSQL")
-##arquivo.write("\nR\n")
-##arquivo.close()
+arquivo = open("RAD em Python/teste.txt", "a")
+arquivo.write("\nSQL")
+arquivo.write("\nR\n")
+arquivo.close()
 #--------------------------------------------------------------------------------------------------------------
 
 #-- 3 ---------------------------------------------------------------------------------------------------------
 #   Caso o modo de escrita seja 'selecionado', e o arquivo pedido não exista, então ele será criado. É importan te lembrar também
 # que, caso o arquivo já exista, com este modo de acesso, o coonteúdo dele será sobrescrito.
 #
-##arquivo = open("RAD em Python/teste2.txt", "w")
-##arquivo.write("Portugol\n")
-##arquivo.write("ADA\n")
-##arquivo.write("C#")
-##arquivo.close()
+arquivo = open("RAD em Python/teste2.txt", "w")
+arquivo.write("Portugol\n")
+arquivo.write("ADA\n")
+arquivo.write("C#")
+arquivo.close()
 #--------------------------------------------------------------------------------------------------------------
 
 #-- 4 ---------------------------------------------------------------------------------------------------------
 #   A remoção de arquivos pode ser feita com a importação do módulo 'os' (de operating system, ou sistema operacional).
 # Esse módulo provê uma interface para funcionalidades dependentes do sistema operacional.
-#
-##import os # Apesar de ser nativo do Python, precisa ser explicitamente importado
-#
+
+import os # Apesar de ser nativo do Python, precisa ser explicitamente importado
+
 #   Verificando se um arquivo existe. Caso positivo, o arquivo é removido. Senão, é impressa a mensagem.
-##if os.path.exists("RAD em Python/teste3.txt"):
-##    os.remove("RAD em Python/teste3.txt")
-##else:
-##    print("ESTO NON ECZISTE")
+if os.path.exists("RAD em Python/teste3.txt"):
+    os.remove("RAD em Python/teste3.txt")
+else:
+    print("ESTO NON ECZISTE")
 #--------------------------------------------------------------------------------------------------------------
 
 #-- 5 ---------------------------------------------------------------------------------------------------------
 # Da mesma forma, ou seja, com o uso do módulo 'os', é possível criar e remover pastas. Entretanto, a remoção só é
 # possível se a pasta estiver vazia.
-#
-##os.mkdir("RAD em Python/TESTE")
-##os.rmdir("RAD em Python") # Só remove se estiver vazia
+
+os.mkdir("RAD em Python/TESTE")
+os.rmdir("RAD em Python") # Só remove se estiver vazia
